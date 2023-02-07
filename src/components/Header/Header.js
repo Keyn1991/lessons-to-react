549fbf9d7cf0dabf5a6033169eadf02eaf37c8ed
dmytro <potapchuk50@gmail.com>
2023-02-07 16:56:09 +0100

    home-work-6 async-thunk

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {useSelector} from "react-redux";
 const Header = () => {
     const {selectedUser} = useSelector(state => state.users )
+    const {selectedPost} = useSelector(state => state.posts )
     return (
         <div className={"header"}>
             {selectedUser && selectedUser.name}
+            {selectedPost && selectedPost.body}
         </div>
     );
 };
